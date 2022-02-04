fa922e53f06e3ad3d260adeacd7ad767bf046a6b
xicooow <xico.dova@gmail.com>
2022-02-04 00:51:41 -0300

    Create essencial components

---
+++ src/components/Output/Output.jsx
@@ -0,0 +1,95 @@
+import {
+  DELIMITERS_REGEXP,
+  TEMPLATE_VARS_REGEXP,
+  ERRORS,
+  replacer
+} from '../../constants';
+
+const {
+  NO_VARS_IN_TEMPLATE,
+  MISSING_VAR,
+  EXTRA_VAR
+} = ERRORS;
+
+const Output = ({
+  output,
+  template,
+  variables,
+  onSetOutput,
+  canRunTemplate
+}) => {
+  const removeDelimiters = (value) => value.replace(DELIMITERS_REGEXP, '');
+
+  const checkErrors = (templateVarsName) => {
+    const missingVars = templateVarsName.reduce((previous, current) => {
+      if (variables.every((val) => val.key !== current)) {
+        previous.push(replacer(MISSING_VAR, [current]));
+      }
+
+      return previous;
+    }, []);
+
+    const extraVars = variables.reduce((previous, current) => {
+      if (templateVarsName.every((val) => val !== current.key)) {
+        previous.push(replacer(EXTRA_VAR, [current.key]));
+      }
+
+      return previous;
+    }, []);
+
+    if (missingVars.length > 0 || extraVars.length > 0) {
+      return {
+        errors: missingVars.concat(extraVars)
+      };
+    }
+
+    return {
+      errors: null
+    };
+  };
+
+  const runTemplate = () => {
+    const templateVars = template.match(TEMPLATE_VARS_REGEXP);
+    if (!templateVars) {
+      onSetOutput(NO_VARS_IN_TEMPLATE);
+      return;
+    }
+
+    const templateVarsName = templateVars.map(removeDelimiters);
+
+    const { errors } = checkErrors(templateVarsName);
+    if (errors) {
+      onSetOutput(errors.join(', '));
+    } else {
+      const result = templateVars.reduce((previous, current) => {
+        const varName = removeDelimiters(current);
+        const varValue = variables.find((variable) => variable.key === varName).value;
+        return previous.replace(current, varValue);
+      }, template);
+
+      onSetOutput(result);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={runTemplate}
+        disabled={!canRunTemplate}
+      >
+        Show Output
+      </button>
+      {
+        (output !== '') &&
+        <div className='display-code'>
+          <code>
+            {`${output}`}
+          </code>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Output;
---
+++ src/components/Variables/index.js
@@ -0,0 +1,2 @@
+import Variables from './Variables';
+export default Variables;
