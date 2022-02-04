40f92a5274a2285d00117f46fc7a6c8c51b2d687
Francisco Neto <10419799+xicooow@users.noreply.github.com>
2022-02-04 01:00:56 -0300

    [Feature] Structural Improvements (#1)
    * Create essencial components
    
    * Fix new line issue

---
+++ src/components/Variables/Variables.jsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+
+import { ERRORS } from '../../constants';
+
+const {
+  UNIQUE_VAR_NAME
+} = ERRORS;
+
+const Variables = ({
+  variables,
+  onAddVariable,
+  onDeleteVariable,
+  onError
+}) => {
+  const [newVarKey, setNewVarKey] = useState('');
+  const [newVarValue, setNewVarValue] = useState('');
+
+  const canAddVariable = () => {
+    return (newVarKey !== '') && (newVarValue !== '');
+  };
+
+  const existingVariableKey = (_key) => {
+    return variables.filter((val) => val.key === _key).length > 0;
+  };
+
+  const addVariable = () => {
+    if (existingVariableKey(newVarKey)) {
+      onError(UNIQUE_VAR_NAME);
+    } else {
+      onAddVariable({
+        key: newVarKey,
+        value: newVarValue
+      });
+      clearInputs();
+    }
+  };
+
+  const clearInputs = () => {
+    setNewVarKey('');
+    setNewVarValue('');
+  };
+
+  return (
+    <>
+      <p><strong>Variables Map</strong></p>
+      <div>
+        <p>
+          <label htmlFor='add-var-key'>Name: </label>
+          <input
+            type='text'
+            id='add-var-key'
+            value={newVarKey}
+            onInput={e => setNewVarKey(e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor='add-var-value'>Value: </label>
+          <input
+            type='text'
+            id='add-var-value'
+            value={newVarValue}
+            onInput={e => setNewVarValue(e.target.value)}
+          />
+        </p>
+        <p>
+          <button
+            type='button'
+            onClick={addVariable}
+            disabled={!canAddVariable()}
+          >
+            Add Variable
+          </button>
+        </p>
+      </div>
+      <div className='display-code'>
+        <code>
+          <ul className='variables-list'>
+            {
+              variables.map((variable, index) => {
+                return (
+                  <li key={index}>
+                    {`"${variable.key}" => "${variable.value}"`}&nbsp;
+                    <button
+                      type='button'
+                      className='link'
+                      onClick={() => onDeleteVariable(variable.key)}
+                    >
+                      <small>delete</small>
+                    </button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </code>
+      </div>
+    </>
+  );
+};
+
+export default Variables;
